f3f5fb8fe45d9cf6d4b342f92c4464b5f47ff030
evgeny <afanasevek@gmail.com>
2020-10-01 22:02:44 +0300

    done

--- Diplom/src/main/java/ru/afanasev/diplom/object/dto/mapper/UserMapper.java
+++ Diplom/src/main/java/ru/afanasev/diplom/object/dto/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package ru.afanasev.diplom.object.dto.mapper;
 import ru.afanasev.diplom.object.Post;
 import ru.afanasev.diplom.object.PostComment;
 import ru.afanasev.diplom.object.User;
+import ru.afanasev.diplom.object.dto.userDtos.UserLoginDtoResponse;
 import ru.afanasev.diplom.object.dto.userDtos.UserNoPhotoDto;
 import ru.afanasev.diplom.object.dto.userDtos.UserWithPhotoDto;
 
@@ -25,6 +26,28 @@ public class UserMapper {
 		user.setPhoto(comment.getUser().getPhoto());
 		
 		return user;
+	}
+
+	public static UserLoginDtoResponse entityToUserLoginDtoResponse(User user, Integer moderationCountPost) {
+		if (user == null) {
+			
+			return null;
+		}
+		UserLoginDtoResponse userDto = new UserLoginDtoResponse();
+		userDto.setId(user.getId());
+		userDto.setEmail(user.getEmail());
+		userDto.setModerationCount(moderationCountPost);
+		if (user.getIsModerator()== -1) {
+			userDto.setModerationCount(0);
+			userDto.setModeration(false);
+			userDto.setSettings(false);
+		} else {
+			userDto.setModeration(true);
+			userDto.setSettings(true);
+		}
+		userDto.setPhoto(user.getPhoto());
+		
+		return userDto;
 	}
 
 }
